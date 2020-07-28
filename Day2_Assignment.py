952bc47cd28d4ae13c61a3a1239b99dd4a10871d
RiyaJain3110 <68581709+RiyaJain3110@users.noreply.github.com>
2020-07-28 09:40:49 +0530

    Create Day2_Assignment.py

---
+++ Day2_Assignment.py
@@ -0,0 +1,64 @@
+print("Hello")
+print('Hello world')
+
+# Using BackSlash 
+print("Using Backslash\n")
+print("Hello \
+         world")
+print("Hello\n world")
+print('Python\'s work')
+
+#Triple quotes
+print("Using triple quotes\n")
+print("""Hello""")
+
+#Using Escape sequence operators
+print("Hello\tWorld")
+
+#String inside quotes
+
+"""
+This is a comment section
+"""
+#Formatted output
+print("Formatted output")
+name="Riya"
+marks=98.7
+age=18
+print("The name of person  is",name,"marks is",marks," age",age)
+print("The name of person  is %4s marks is %4f age is %4d"%(name, marks, age))
+
+#Operators
+Print("Operators")
+a=10
+b=20
+#Arithmetic Operators
+print("Arithmetic Operators")
+c=a+b
+print(c)
+c=a-b
+print(c)
+c=a*b
+print(c)
+c=b/a
+print(c)
+c=b//a
+print(c)
+c=a**b
+print(c)
+
+#Assignment Operators
+print("Assignment Operators")
+a=b
+a+=b
+print(" After first assignment a is",a)
+a*=b
+print(" After second assignment a is",a)
+
+#Comparision Operators
+print("Comparision operators")
+a=5
+b=8
+print(a==b)
+print(a<b)
+print(not a<b)
