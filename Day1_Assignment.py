1432ab8c264042a9588b1270c3d809e7547e3808
RiyaJain3110 <68581709+RiyaJain3110@users.noreply.github.com>
2020-07-28 09:36:23 +0530

    Create Day1_Assignment.py

---
+++ Day1_Assignment.py
@@ -0,0 +1,16 @@
+1. What is python?What are ways to install python in your PC
+Ans- 
+Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum , first released in 1991 and became most popular programming language in 2006.
+Ways to install python
+I) In windows
+II) In Linux
+III) In MacOS
+IV) Using Anaconda distribution
+V) Using Pycharm, Visual studio, Windows powershell..
+
+2.What is the code environment? Types?
+Ans - Code environment is a place where we can write our program, compile the program and see the performance of our program i.e output of our program all in one place.
+There are three types of code environment
+I).  Text editor
+II).  Full IDEs( IDE meanse Integrated Development Environment)
+III). Notebook environments.
